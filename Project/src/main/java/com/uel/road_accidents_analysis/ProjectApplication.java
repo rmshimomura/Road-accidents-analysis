b01e0dadfb31db9abe602c70accd573cc5a68e52
rmshimomura <shimomurarodrigo1@gmail.com>
2022-10-30 00:24:33 -0300

    Adicionado TODOS para facilitar implmentações

--- Project/src/main/java/com/uel/road_accidents_analysis/ProjectApplication.java
+++ Project/src/main/java/com/uel/road_accidents_analysis/ProjectApplication.java
@@ -2,12 +2,23 @@ package com.uel.road_accidents_analysis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProjectApplication {
+public class ProjectApplication extends SpringBootServletInitializer {
+
+    /*
+       TODO Remontar banco de dados
+       TODO Implementar DAOs
+       TODO LogController
+       TODO Tratamento de arquivo
+    * */
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ProjectApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
     }
