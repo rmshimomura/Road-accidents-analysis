a03a6431fce7f3184fe5cfc0737553ec7678ff7f
felipe based mito <42080804+felipeyousoro@users.noreply.github.com>
2022-11-26 19:12:15 -0300

    Gráficos casualidades

--- Project/src/main/java/com/uel/road_accidents_analysis/dao/interfaces/custom/CasualidadeNoAcidenteDAO.java
+++ Project/src/main/java/com/uel/road_accidents_analysis/dao/interfaces/custom/CasualidadeNoAcidenteDAO.java
@@ -2,11 +2,14 @@ package com.uel.road_accidents_analysis.dao.interfaces.custom;
 
 import com.uel.road_accidents_analysis.dao.interfaces.base.DAO;
 import com.uel.road_accidents_analysis.model.CasualidadeNoAcidente;
+import com.uel.road_accidents_analysis.model.CasualidadeNoAcidenteTotal;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public interface CasualidadeNoAcidenteDAO extends DAO<CasualidadeNoAcidente> {
 
     public CasualidadeNoAcidente getByDoubleId(Long id_acidente_cc, Long id_tipo_casualidade) throws SQLException;
 
+    public List<CasualidadeNoAcidenteTotal> getSumCasualidadesBySeverity() throws SQLException;
 }
--- Project/src/main/java/com/uel/road_accidents_analysis/dao/interfaces/custom/TipoCasualidadeDAO.java
+++ Project/src/main/java/com/uel/road_accidents_analysis/dao/interfaces/custom/TipoCasualidadeDAO.java
@@ -3,6 +3,9 @@ package com.uel.road_accidents_analysis.dao.interfaces.custom;
 import com.uel.road_accidents_analysis.dao.interfaces.base.DAO;
 import com.uel.road_accidents_analysis.model.TipoCasualidade;
 
+import java.util.List;
+
 public interface TipoCasualidadeDAO extends DAO<TipoCasualidade> {
+
 
 }
