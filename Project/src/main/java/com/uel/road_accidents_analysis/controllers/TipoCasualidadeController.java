2990b38211db9d0b8374f242981d6cf353aa2d58
rmshimomura <shimomurarodrigo1@gmail.com>
2022-11-26 20:11:59 -0300

    Reorganização

--- Project/src/main/java/com/uel/road_accidents_analysis/controllers/TipoCasualidadeController.java
+++ Project/src/main/java/com/uel/road_accidents_analysis/controllers/TipoCasualidadeController.java
@@ -1,16 +1,51 @@
 package com.uel.road_accidents_analysis.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.uel.road_accidents_analysis.dao.factories.DAOFactory;
+import com.uel.road_accidents_analysis.model.CasualidadeNoAcidenteTotal;
+import org.primefaces.model.chart.PieChartModel;
 
-@Controller
-@RequestMapping("/casualidades")
-public class TipoCasualidadeController {
+import javax.annotation.PostConstruct;
+import javax.faces.bean.RequestScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
 
-    @GetMapping
-    public String getTipoCasualidades() {
-        return "jsp/casualidades";
+@Named
+@RequestScoped
+public class TipoCasualidadeController implements Serializable {
+
+    private PieChartModel casualidadeNoAcidentePieChart;
+
+    @PostConstruct
+    public void init() {
+        createCasualidadeNoAcidentePieChart();
     }
+
+    public void createCasualidadeNoAcidentePieChart() {
+        try(DAOFactory daoFactory = DAOFactory.getInstance()) {
+            casualidadeNoAcidentePieChart = new PieChartModel();
+            List<CasualidadeNoAcidenteTotal> casualidades = daoFactory.getCasualidadeNoAcidenteDAO().getSumCasualidadesBySeverity();
+
+            for (CasualidadeNoAcidenteTotal casualidade : casualidades) {
+                casualidadeNoAcidentePieChart.set(casualidade.getNomeCasualidade(), casualidade.getTotal());
+            }
+
+            casualidadeNoAcidentePieChart.setTitle("Casualidades por gravidade");
+            casualidadeNoAcidentePieChart.setLegendPosition("w");
+            casualidadeNoAcidentePieChart.setShowDataLabels(true);
+            casualidadeNoAcidentePieChart.setDiameter(300);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public PieChartModel getCasualidadeNoAcidentePieChart() {
+        return casualidadeNoAcidentePieChart;
+    }
+
+
+
 }
