ae44a51816cc3fcbca24f8bd8942c09f9b74d5a7
felipe based mito <42080804+felipeyousoro@users.noreply.github.com>
2022-11-29 11:23:09 -0300

    Graficos de Acidentes por rodovias

--- Project/src/main/java/com/uel/road_accidents_analysis/dao/interfaces/custom/AcidenteComCasualidadeDAO.java
+++ Project/src/main/java/com/uel/road_accidents_analysis/dao/interfaces/custom/AcidenteComCasualidadeDAO.java
@@ -3,12 +3,16 @@ package com.uel.road_accidents_analysis.dao.interfaces.custom;
 import com.uel.road_accidents_analysis.dao.interfaces.base.DAO;
 import com.uel.road_accidents_analysis.model.AcidenteComCasualidade;
 import com.uel.road_accidents_analysis.model.AcidenteSemCasualidade;
+import com.uel.road_accidents_analysis.model.query_aux.AcidentesRodoviaCount;
 
 import java.sql.SQLException;
 import java.sql.Time;
 import java.util.Date;
+import java.util.List;
 
 public interface AcidenteComCasualidadeDAO extends DAO<AcidenteComCasualidade> {
     public AcidenteComCasualidade getByTrechoHorarioDataAndKm(Long idTrecho, Date data, Time horario, Double km) throws SQLException;
+
+    public List<AcidentesRodoviaCount> getCountAcidentesForEachRodovia() throws SQLException;
 
 }
--- Project/src/main/java/com/uel/road_accidents_analysis/dao/interfaces/custom/AcidenteSemCasualidadeDAO.java
+++ Project/src/main/java/com/uel/road_accidents_analysis/dao/interfaces/custom/AcidenteSemCasualidadeDAO.java
@@ -2,11 +2,15 @@ package com.uel.road_accidents_analysis.dao.interfaces.custom;
 
 import com.uel.road_accidents_analysis.dao.interfaces.base.DAO;
 import com.uel.road_accidents_analysis.model.AcidenteSemCasualidade;
+import com.uel.road_accidents_analysis.model.query_aux.AcidentesRodoviaCount;
 
 import java.sql.SQLException;
 import java.sql.Time;
 import java.util.Date;
+import java.util.List;
 
 public interface AcidenteSemCasualidadeDAO extends DAO<AcidenteSemCasualidade> {
     public AcidenteSemCasualidade getByTrechoHorarioDataAndKm(Long idTrecho, Date data, Time horario, Double km) throws SQLException;
+
+    public List<AcidentesRodoviaCount> getCountAcidentesForEachRodovia() throws SQLException;
 }
